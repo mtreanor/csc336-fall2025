15daf0a31f69829a5a436db045f99b1915598d17
Mike Treanor <treanor@american.edu>
2025-08-04 15:00:06 -0700

    Fix GitHub Pages configuration and build static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
-    images: {
-      unoptimized: true
-    }
-  };
-  
-  export default nextConfig;
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/csc336-fall2025' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/csc336-fall2025' : ''
+};
+
+export default nextConfig;
