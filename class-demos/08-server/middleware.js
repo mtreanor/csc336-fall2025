919c86e4d9f1d9d6c7b1e0a2d31d2f88b38520dd
Mike Treanor <treanor@american.edu>
2025-10-13 11:50:31 -0400

    added file example to render server

--- class-demos/08-server/middleware.js
+++ class-demos/08-server/middleware.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import fs from "fs";
+
 const app = express()
 
 const requestTime = function (req, res, next) {
@@ -11,10 +13,20 @@ app.use(express.static("public"));
 app.use(requestTime)
 
 app.get('/test', (req, res) => {
-  let responseText = 'Hello World!<br>'
-  responseText += `<small>Requested at: ${req.requestTime}</small>`
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.json(responseText)
+  let times = [];
+
+  try {
+    let fileContents = fs.readFileSync("times.json", "utf-8");
+    times = JSON.parse(fileContents);
+  } catch (error) {
+    console.log("ERROR: fs.readFileSync threw an error. 'program_count.txt' doesn't exist.");
+  }
+
+  times.push(Date.now());
+
+  fs.writeFileSync("times.json", JSON.stringify(times));
+
+  res.json(times)
 })
 
 app.listen(3000);
